83b0d68570bbedcb27380fdd5fd0089158355fd3
Kamal Adel <Kamal.adel@outlook.com>
2026-08-30 18:45:40 +0300

    feat(app): custom app icon + apps-screen entry with Treasury workspace route

--- treasury/hooks.py
+++ treasury/hooks.py
@@ -11,15 +11,14 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "treasury",
-# 		"logo": "/assets/treasury/logo.png",
-# 		"title": "Treasury",
-# 		"route": "/treasury",
-# 		"has_permission": "treasury.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "treasury",
+		"logo": "/assets/treasury/images/app_icon.png",
+		"title": "Treasury",
+		"route": "/app/treasury",
+	}
+]
 
 # Includes in <head>
 # ------------------
